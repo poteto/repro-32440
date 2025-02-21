203f02c1164a653fddb8c41a26378a3ac5bd45cb
Lauren Tan <poteto@users.noreply.github.com>
2025-02-21 11:22:17 -0500

    replicate some deps

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = (api) => {
+  api.cache(true);
+
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      [
+        "babel-plugin-react-docgen-typescript",
+        {
+          exclude: "node_modules",
+          include: "components.*\\.tsx$",
+        },
+      ],
+    ],
+  };
+};
